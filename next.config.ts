8d4b0290dc8416177760ff6cc7acdeb8617ce022
shonoshono <shono131103@gmail.com>
2026-02-21 21:20:28 +0900

    add: origin設定を明示的に追加

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ["localhost", "192.168.5.243"],
 };
 
 export default nextConfig;
